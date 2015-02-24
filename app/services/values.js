0f6d20626776adc63813bdbcafb29b27783d02c3
Tony Hobbins <mail@tonyhobbins.com>
2015-02-24 14:11:30 +1100

    updated port number to 8080

--- app/services/values.js
+++ app/services/values.js
@@ -1,6 +1,6 @@
 angular.module('harvesterApp').value('appSettings',{
     title : 'Harvester Application',
     version : '0.1',
-    urlbase : 'http://harvester.ironcache.net:9000',
+    urlbase : 'http://harvester.ironcache.net:8080',
     apiModule : 'controller_api'
 });
