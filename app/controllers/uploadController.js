5e59fccb517367adf1053a1d4e990e8a2af3d5e8
Tony Hobbins <mail@tonyhobbins.com>
2015-02-25 17:35:48 +1100

    dynamic class for upload button

--- app/controllers/uploadController.js
+++ app/controllers/uploadController.js
@@ -2,13 +2,12 @@
 
     var uploadController = function ($scope, $upload, apiFactory,  appSettings) {
         var defaultLabel = "Import File";
-        var defaultClass = "btn btn-primary btn-block";
-        var loadingClass = "btn btn-secondary btn-block";
+
         $scope.appSettings = appSettings;
         $scope.progress = 0;
         $scope.formData = "";
+        $scope.isUploading = false;
         $scope.button_label = defaultLabel;
-        $scope.button_class = defaultClass;
         $scope.onFileSelect = function ($files) {
             for (var i = 0; i < $files.length; i++) {
                 var file = $files[i];
@@ -20,13 +19,14 @@
                     ).progress(function (evt) {
                         var pct = parseInt(100.0 * evt.loaded / evt.total);
                         $scope.progress = pct;
+                        $scope.isUploading = true;
                         $scope.button_label = pct + "%";
-                        $scope.button_class = loadingClass;
+
                         //console.log('percent: ' + pct);
                     })
                     .success(function (data, status, headers, config) {
                         $scope.button_label = defaultLabel;
-                        $scope.button_class = defaultClass;
+                        $scope.isUploading = false;
                         //console.log(data);
                         $scope.$emit('refresh', {message: "refresh"});
                     });
