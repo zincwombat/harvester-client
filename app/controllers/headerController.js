52c2a95b39be7ff9dd28439a35e8d8a27d83b137
Tony Hobbins <mail@tonyhobbins.com>
2015-02-27 14:42:17 +1100

    changed menu handling, used headerController

---
+++ app/controllers/headerController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by tonyhobbins on 27/02/15.
+ */
+(function () {
+
+    var headerController = function ($scope,$location) {
+            $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+
+    headerController.$inject = ['$scope','$location'];
+
+    angular.module('harvesterApp')
+        .controller('headerController', headerController);
+}}());
