79e95a9bbb6fab348d56cb472f3e1c783f19fff1
Tony Hobbins <mail@tonyhobbins.com>
2015-02-27 14:47:13 +1100

    changed menu handling, used headerController

--- app/controllers/headerController.js
+++ app/controllers/headerController.js
@@ -5,7 +5,7 @@
 
     var headerController = function ($scope,$location) {
             $scope.isActive = function (viewLocation) {
-            return viewLocation === $location.path();
+                return viewLocation === $location.path();
         };
 
     headerController.$inject = ['$scope','$location'];
